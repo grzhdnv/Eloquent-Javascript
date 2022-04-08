65f92062d8cacc1b9c238d249c6a790c73176239
mgrzhdnv <mgrazhdanov@gmail.com>
2022-04-08 20:37:18 +0300

    recursion done

--- 03-Functions/recursion.js
+++ 03-Functions/recursion.js
@@ -11,3 +11,14 @@
 // a Boolean.
 // Test it on 50 and 75. See how it behaves on -1. Why? Can you think of a
 // way to fix this?
+
+function isEven(n) {
+  if (n === 0) return true;
+  else if (n === 1) return false;
+  else if (n < 0) return isEven(-n); // deals with negative numbers
+  else if (n >= 2) return isEven(n - 2);
+}
+
+console.log(isEven(50));
+console.log(isEven(75));
+console.log(isEven(-1));
