ab214ecbffefea5ec95c650430294b4b7331c0c4
mgrzhdnv <mgrazhdanov@gmail.com>
2022-04-24 12:51:54 +0300

    Chapter 4 done

---
+++ 04-DataStructures_ObjectsAndArrays/reversing-an-array.js
@@ -0,0 +1,29 @@
+// Arrays have a reverse method that changes the array by inverting the order in
+// which its elements appear. For this exercise, write two functions, reverseArray
+// and reverseArrayInPlace. The first, reverseArray, takes an array as argument
+// and produces a new array that has the same elements in the inverse order. The
+// second, reverseArrayInPlace, does what the reverse method does: it modifies
+// the array given as argument by reversing its elements. Neither may use the
+// standard reverse method.
+// Thinking back to the notes about side effects and pure functions in the
+// previous chapter, which variant do you expect to be useful in more situations?
+// Which one runs faster?
+
+const test = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+const reverseArray = (array) => {
+  const newArray = [];
+  array.forEach((x) => newArray.unshift(x));
+  return newArray;
+};
+
+const reverseArrayInPlace = (array) => {
+  for (let i = 0; i < Math.floor(array.length / 2); i++) {
+    let temp = array[i];
+    array[i] = array[array.length - 1 - i];
+    array[array.length - 1 - i] = temp;
+  }
+  return array;
+};
+
+console.log(reverseArrayInPlace(test));
