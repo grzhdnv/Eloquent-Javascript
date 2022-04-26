e07db5100cc451ebaa3cc24d5a806cd12e29d3c8
mgrzhdnv <mgrazhdanov@gmail.com>
2022-04-26 15:20:11 +0300

    line comments to block comments change, ch.6 promts added, recommit

--- 02-Program_Structure/chessboard.js
+++ 02-Program_Structure/chessboard.js
@@ -1,28 +1,28 @@
-// Write a program that creates a string that represents an 8×8 grid, using newline
-// characters to separate lines. At each position of the grid there is either a space
-// or a "#" character. The characters should form a chessboard.
-// Passing this string to console.log should show something like this:
+/* Write a program that creates a string that represents an 8×8 grid, using newline
+characters to separate lines. At each position of the grid there is either a space
+or a "#" character. The characters should form a chessboard.
+Passing this string to console.log should show something like this:
 
-// # # # #
-//  # # # #
-// # # # #
-// # # # #
-//  # # # #
-// # # # #
-//  # # # #
-// # # # #
+# # # #
+ # # # #
+# # # #
+# # # #
+ # # # #
+# # # #
+ # # # #
+# # # #
 
-// When you have a program that generates this pattern, define a binding size
-// = 8 and change the program so that it works for any size, outputting a grid
-// of the given width and height.
+When you have a program that generates this pattern, define a binding size
+= 8 and change the program so that it works for any size, outputting a grid
+of the given width and height.
 
-// let str = ``;
-// const firstString = "##  ##  ##  ##  ";
-// const secondString = "  ##  ##  ##  ##";
-// for (let i = 0; i < 4; i++) {
-//   str += `${firstString}\n${secondString}\n`;
-// }
-// console.log(str);
+let str = ``;
+const firstString = "##  ##  ##  ##  ";
+const secondString = "  ##  ##  ##  ##";
+for (let i = 0; i < 4; i++) {
+  str += `${firstString}\n${secondString}\n`;
+}
+console.log(str); */
 
 const size = 20;
 let str = ``;
--- 03-Functions/minimum.js
+++ 03-Functions/minimum.js
@@ -1,6 +1,6 @@
-// The previous chapter introduced the standard function Math.min that returns
-// its smallest argument. We can build something like that now. Write a function
-// min that takes two arguments and returns their minimum.
+/* The previous chapter introduced the standard function Math.min that returns
+its smallest argument. We can build something like that now. Write a function
+min that takes two arguments and returns their minimum. */
 
 function findMin(a, b) {
   if (a < b) return a;
